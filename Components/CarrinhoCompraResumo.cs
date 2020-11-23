e2446f750f7e38d10abfc5420ed8a78e244b4b8b
renancosta.dsn.shop <renancosta.dsn.shop@alterdata.com.br>
2020-11-23 20:59:11 -0300

    Criação do componente de carrinho de compras

---
+++ Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,33 @@
+using LanchesMac.Models;
+using LanchesMac.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        public CarrinhoCompra _carrinhoCompra { get; set; }
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //_carrinhoCompra.CarrinhoCompraItens = _carrinhoCompra.GetCarrinhoCompraItems();
+            _carrinhoCompra.CarrinhoCompraItens = new System.Collections.Generic.List<CarrinhoCompraItem>()
+            {
+                new CarrinhoCompraItem(), new CarrinhoCompraItem()
+            };
+
+            var vwCarrinhoCompra = new CarrinhoCompraViewModel()
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(vwCarrinhoCompra);
+        }
+
+    }
+}
